058c9114e8ee583707988f1a8d5410e1e53b1731
Manjeet singh <74854072+kaal0408@users.noreply.github.com>
2022-02-20 22:55:56 +0530

    Update yourstepsis.py

--- yourstepsis.py
+++ yourstepsis.py
@@ -1,6 +1,6 @@
 from os import system
 system("git clone https://gitHub.com/kaal0408/Meow2.0 && cd Meow2.0 && python3 -m userbot")
 # if got error
-print(" Sʀʏʏ ᴜɴғᴏʀᴛᴜɴᴀᴛᴇʟʏ ʏᴏᴜ ʜᴀᴠᴇ ɢᴏᴛ ᴇʀʀᴏʀ ᴘʟᴇᴀsᴇ ʀᴇᴘᴏʀᴛ ɪᴛ ᴀᴛ  @Murat_30_God)
+#print(" Sʀʏʏ ᴜɴғᴏʀᴛᴜɴᴀᴛᴇʟʏ ʏᴏᴜ ʜᴀᴠᴇ ɢᴏᴛ ᴇʀʀᴏʀ ᴘʟᴇᴀsᴇ ʀᴇᴘᴏʀᴛ ɪᴛ ᴀᴛ  @Murat_30_God)
 # Script written for stop heroku suspension
 # use with credit else gay
