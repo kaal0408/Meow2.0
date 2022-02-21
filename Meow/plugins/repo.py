5604ca4dca8e9abc9df2bc87cfff817fbb0603bb
Manjeet singh <74854072+kaal0408@users.noreply.github.com>
2022-02-21 22:35:25 +0530

    Update repo.py

--- Meow/plugins/repo.py
+++ Meow/plugins/repo.py
@@ -3,8 +3,8 @@
 async def _(event):
     if event.fwd_from:
         return
-    Repo = f"[Click Here](https://github.com/Copycat-izz-opp/CopyCat_Spam_Bot)"
-    Deploy = f"[Click Here](https://dashboard.heroku.com/new?template=https%3A%2F%2Fgithub.com%2FYukkiBot%2FYukkiXdeploy)"
+    Repo = f"[Click Here](https://github.com/kaal0408/Meow2.0)"
+    Deploy = f"[Click Here](https://dashboard.heroku.com/new?template=https%3A%2F%2Fgithub.com%2kaal0408%2FMeow2.0)"
     await edit_or_reply(
-        event, f"**CopyCat Spam Bot Repo:** {Repo}\n\n**Deploy Now:** {Deploy}"
+        event, f"**Meow 2.0 userbot Repo:** {Repo}\n\n**Deploy Now:** {Deploy}"
     )
